5576080cf5e721192f38bbd3f6eb1efea4e072ec
gugrug <gustavorugg@gmail.com>
2020-10-17 21:50:15 -0300

    Seg fault and double free corrected. s_line with scope problens

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gumartin <gumartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/31 04:04:34 by gumartin          #+#    #+#             */
-/*   Updated: 2020/10/15 06:11:24 by gumartin         ###   ########.fr       */
+/*   Updated: 2020/10/17 21:42:31 by gumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,32 @@ int	get_next_line(int fd, char **line)
 {
 	static char	*s_line;
 	char	*buf;
-	int	rd, i_test;
+	int	rd,
 	
 	i_test = 0;
 	if (fd < 0 || !(line) || BUFFER_SIZE < 1)
 		return (-1);
 	buf = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char));								//malloc buf 1
 	ft_strclean(buf, ft_strlen(buf));
+
 	if (!(s_line))
 		s_line = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char*));						//malloc s_line 2
-	while ((rd = read(fd, buf, BUFFER_SIZE > 0)) && !(ft_strchr(s_line, '\n')) && i_test < 10)
+	while ((rd = read(fd, buf, BUFFER_SIZE) > 0) && !(ft_strchr(s_line, '\n')))
 	{
-		write(1, "\n-2-\n", 4); 	
+		printf("O valor de buf e: %s \n", buf);
 		ft_putbuf(buf, s_line);
-		i_test++;
 		if(ft_strchr(s_line, '\n'))
+		{
+			write(1, "\n-b-\n", 5);
 			break;
+		}
 	}
 	ft_puts_line(s_line, line);
 	free(buf);													//buf free 1	
+	buf = NULL;
 	free(s_line);												//s_line free 2
+	s_line = NULL;
 	//tests
-	write(1, "\n-1-\n", 4);
 	return(0);
 }
 
@@ -50,7 +54,7 @@ void	ft_putbuf(char *buf, char *s_line)
 	int p;
 
 	if ((bn = ft_strchr(buf, '\n')))
-		p = buf - bn;
+		p = ft_strlen(buf) - ft_strlen(bn);
 	else
 		p = ft_strlen(buf);
 	cpy = (char*)malloc((p + 1)* sizeof(char*));				//malloc cpy 3
@@ -58,7 +62,8 @@ void	ft_putbuf(char *buf, char *s_line)
 	s_line = ft_strjoin(s_line, cpy);
 	if (ft_strchr(buf, '\n')){
 		free(cpy);													//cpy free 3
-	write(1, "\n-4-\n", 4); }		
+		cpy = NULL;
+	}
 }
 
 void	ft_puts_line(char *s_line, char **line)
@@ -66,12 +71,12 @@ void	ft_puts_line(char *s_line, char **line)
 	//put s_line in line, than free s_line pointing to '\n'
 	int	i;
 	int	pbn;
-	
 	i = 0;
 	pbn = ft_strlen(s_line);
 	if (ft_strchr(s_line, '\n'))
-		pbn = ft_strchr(s_line, '\n') - s_line;	
+		pbn = ft_strlen(ft_strchr(s_line, '\n')) - ft_strlen(s_line);	
 	ft_strlcpy(*line, s_line, pbn + 1);
+		write(1, "\n-A-\n", 5);
 	while (i < pbn)
 	{
 		s_line++;
@@ -92,10 +97,10 @@ int	main(void)
 	for(i = 0; i < 3; i++)
 	{
 		get_next_line(open("text.txt", 0), &line); 
-		write(1, "\n-3-\n", 4); 	
-		line[1] = 'B';
+		write(1, "\n-3-\n", 5); 	
 		printf("\nLine is: %s ---\n", line);
 		write(1, line, (ft_strlen(line)));
+		write(1, "\n-4-\n", 5); 	
 	}
 	write(1, "end\n", 4);
 	return(0);
