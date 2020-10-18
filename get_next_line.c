5ded1ef8a3bdbb7ae43b131de08eca3a578f103f
gugrug <gustavorugg@gmail.com>
2020-10-17 22:50:38 -0300

    s_line still with problems, printf puts for debugging

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: gumartin <gumartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/31 04:04:34 by gumartin          #+#    #+#             */
-/*   Updated: 2020/10/17 21:42:31 by gumartin         ###   ########.fr       */
+/*   Updated: 2020/10/17 22:48:09 by gumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#define BUFFER_SIZE 6
+#define BUFFER_SIZE 5
 
 int	get_next_line(int fd, char **line)
 {
@@ -25,25 +25,25 @@ int	get_next_line(int fd, char **line)
 		return (-1);
 	buf = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char));								//malloc buf 1
 	ft_strclean(buf, ft_strlen(buf));
-
 	if (!(s_line))
+	{
 		s_line = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char*));						//malloc s_line 2
-	while ((rd = read(fd, buf, BUFFER_SIZE) > 0) && !(ft_strchr(s_line, '\n')))
+		ft_strclean(s_line, ft_strlen(s_line));
+	}
+	while ((rd = read(fd, buf, BUFFER_SIZE) > 0)) //&& !(ft_strchr(s_line, '\n')))
 	{
 		printf("O valor de buf e: %s \n", buf);
+		printf("O valor de s_line no GNL ao entrar e: %s \n", s_line);
 		ft_putbuf(buf, s_line);
+		printf("O valor de s_line no GNL ao Sair e: %s \n\n", s_line);
 		if(ft_strchr(s_line, '\n'))
-		{
-			write(1, "\n-b-\n", 5);
 			break;
-		}
 	}
 	ft_puts_line(s_line, line);
 	free(buf);													//buf free 1	
 	buf = NULL;
 	free(s_line);												//s_line free 2
 	s_line = NULL;
-	//tests
 	return(0);
 }
 
@@ -52,18 +52,18 @@ void	ft_putbuf(char *buf, char *s_line)
 	char *bn;
 	char *cpy;
 	int p;
-
+	
+	printf("O valor de s_line ao entrar e: %s \n", s_line);
 	if ((bn = ft_strchr(buf, '\n')))
 		p = ft_strlen(buf) - ft_strlen(bn);
 	else
 		p = ft_strlen(buf);
 	cpy = (char*)malloc((p + 1)* sizeof(char*));				//malloc cpy 3
-	ft_strlcpy(cpy, buf, p);
+	ft_strlcpy(cpy, buf, p + 1);
 	s_line = ft_strjoin(s_line, cpy);
-	if (ft_strchr(buf, '\n')){
-		free(cpy);													//cpy free 3
-		cpy = NULL;
-	}
+	free(cpy);													//cpy free 3
+	cpy = NULL;
+	printf("O valor de s_line ao sair e: %s \n", s_line);
 }
 
 void	ft_puts_line(char *s_line, char **line)
@@ -76,7 +76,6 @@ void	ft_puts_line(char *s_line, char **line)
 	if (ft_strchr(s_line, '\n'))
 		pbn = ft_strlen(ft_strchr(s_line, '\n')) - ft_strlen(s_line);	
 	ft_strlcpy(*line, s_line, pbn + 1);
-		write(1, "\n-A-\n", 5);
 	while (i < pbn)
 	{
 		s_line++;
@@ -94,10 +93,9 @@ int	main(void)
 	int	i = 0;
 	char *line;
 	write(1, "gnl:\n", 5);
-	for(i = 0; i < 3; i++)
+	for(i = 0; i < 1; i++)
 	{
 		get_next_line(open("text.txt", 0), &line); 
-		write(1, "\n-3-\n", 5); 	
 		printf("\nLine is: %s ---\n", line);
 		write(1, line, (ft_strlen(line)));
 		write(1, "\n-4-\n", 5); 	
